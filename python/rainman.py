95a2ff1ee2fd27344f9ff3860f95395f1b3ee8eb
Joost Verberk <joost.verberk@gmail.com>
2017-07-13 19:22:31 +0200

    First attempt at Pickle

--- python/rainman.py
+++ python/rainman.py
@@ -4,6 +4,7 @@ import datetime as dt
 import logging
 import sys
 from time import sleep
+import pickle
 
 # Weather Underground API
 base = "http://api.wunderground.com/api/f0c3002dabae8c04/"
@@ -19,16 +20,16 @@ lim_high_temp = 30 # C
 lim_med_temp = 25 # C
 lim_low_temp = 20 # C
 lim_days_ahead_high_temp = 1 # days
-lim_days_ahead_med_temp = 2 # days
+lim_days_ahead_med_temp = 1 # days
 lim_days_ahead_low_temp = 3 # days
-lim_qpf_high_temp = 3 # mm
+lim_qpf_high_temp = 5 # mm
 lim_qpf_med_temp = 3 # mm
 lim_qpf_low_temp = 3 # mm
 
 # Sprinkler modes
-high_duration = 8 # mins
-med_duration = 8 # mins
-low_duration = 5 # mins
+high_duration = 10 # mins
+med_duration = 10 # mins
+low_duration = 10 # mins
 high_times = [8, 11, 14, 17, 20]
 med_times = [8, 13, 18]
 low_times = [8, 18]
@@ -46,7 +47,7 @@ base_path = '/media/usb/log/'
 logfile = base_path + 'output.log'
 log_level = logging.INFO
 logging.basicConfig(format='%(asctime)s [%(filename)s] [%(levelname)s] %(message)s', level=log_level, datefmt='%Y-%m-%d %H:%M:%S', filename=logfile)
-logging.info('=== START OF SESSION ===')
+#logging.info('=== START OF SESSION ===')
 
 # Verbose
 verbose = False
@@ -75,7 +76,7 @@ if(verbose):
 
 # Process forecast to sprinkler mode
 if (today_high_temp > lim_high_temp):
-    qpf_days_ahead = sum(qpf_allday[1:lim_days_ahead_high_temp])
+    qpf_days_ahead = sum(qpf_allday[0:lim_days_ahead_high_temp])
     lim_days_ahead = lim_days_ahead_high_temp
     if (qpf_days_ahead < lim_qpf_high_temp):
         # Very hot and no rain today so high sprinkler mode
@@ -83,7 +84,7 @@ if (today_high_temp > lim_high_temp):
         sprinkler_times = high_times
         sprinkler_mode = 'high'
 elif (today_high_temp > lim_med_temp):
-    qpf_days_ahead = sum(qpf_allday[1:lim_days_ahead_med_temp])
+    qpf_days_ahead = sum(qpf_allday[0:lim_days_ahead_med_temp])
     lim_days_ahead = lim_days_ahead_med_temp
     if (qpf_days_ahead < lim_qpf_med_temp):
         # Hot and no rain today so medium sprinkler mode
@@ -91,7 +92,7 @@ elif (today_high_temp > lim_med_temp):
         sprinkler_times = med_times
         sprinkler_mode = 'medium'
 elif (today_high_temp > lim_low_temp):
-    qpf_days_ahead = sum(qpf_allday[1:lim_days_ahead_low_temp])
+    qpf_days_ahead = sum(qpf_allday[0:lim_days_ahead_low_temp])
     lim_days_ahead = lim_days_ahead_low_temp
     if (qpf_days_ahead < lim_qpf_low_temp):
         # Warm and no rain next x days so low sprinkler mode
@@ -100,20 +101,44 @@ elif (today_high_temp > lim_low_temp):
         sprinkler_mode = 'low'
 
 if(verbose):
-    print "Next %d days rain forecast in total is: %s mm" % (lim_days_ahead, qpf_days_ahead)
+    print "Next %d days rain forecast in total is: [%s] mm" % (lim_days_ahead, str(qpf_allday)[1:3*lim_days_ahead-1])
     print "Sprinkler mode for today is: %s" % (sprinkler_mode)
 
 # Control actuators
 hour = dt.datetime.now().hour
-if (hour == 0):
+
+if (hour == 6):
     # Write daily forecast summary to logging
-    logging.info("Todays maximum temperature is: %dC" % (today_high_temp))
+    logging.info("= DAILY WEATHER FORECAST =")
+    logging.info("Forecasted maximum temperature is: %dC" % (today_high_temp))
     logging.info("Next 10 days rain forecast per day is: [%s] mm" % (str(qpf_allday)[1:-1]))
-    logging.info("Next %d days rain forecast in total is: %s mm" % (lim_days_ahead, qpf_days_ahead))
+    logging.info("Next %d days rain forecast in total is: [%s] mm" % (lim_days_ahead, str(qpf_allday)[1:3*lim_days_ahead-1]))
     logging.info("Sprinkler mode for today is: %s" % (sprinkler_mode))
+    sprinkler_info = [today_high_temp,qpf_allday,lim_days_ahead,sprinkler_mode,sprinkler_duration,sprinkler_times]
+    #fp = open('sprinkler.mode', 'w')
+    #for item in sprinkler_info:
+    #    fp.write("%s\n" % item)
+    #fp.close()
+
+# Read sprinkler information from file
+#fp = open('sprinkler.mode', 'r')
+#sprinkler_info = fp.read().splitlines()
+#fp.close
+
+with open('sprinkler.pickle', 'w') as fp:
+    pickle.dump([today_high_temp, qpf_allday], fp)
+
+with open('sprinkler.pickle') as fp:
+    today_high_temp, qpf_allday = pickle.load(fp)
+
+print today_high_temp
+print qpf_allday
 
 if (hour in sprinkler_times):
     try:
+        logging.info("Latest maximum temperature forecast is: %dC" % (today_high_temp))
+        logging.info("Latest %d days rain forecast in total is: [%s] mm" % (lim_days_ahead, str(qpf_allday)[1:3*lim_days_ahead-1]))
+        logging.info("Sprinkler mode is: %s" % (sprinkler_mode))
         f = urllib.urlopen(sprinkler_on)
         logging.info("Sprinklers enabled for %d minutes [%s mode]" % (sprinkler_duration, sprinkler_mode))
         sleep(sprinkler_duration*60)
@@ -126,4 +151,4 @@ if (hour in sprinkler_times):
 #    logging.info("No action required [%s mode]" % (sprinkler_mode))
 
 # End program
-logging.info('=== END OF SESSION ===')
+#logging.info('=== END OF SESSION ===')
