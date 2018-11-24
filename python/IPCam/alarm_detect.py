5e37bf76d7d2ccbcca929be518173f78c1688c8f
LAP-EHV-JV\Joost <joost.verberk@gmail.com>
2018-11-24 10:57:50 +0100

    V1 release of alarm_detect with Foscam and Pushover integration (including readme!)

--- python/IPCam/alarm_detect.py
+++ python/IPCam/alarm_detect.py
@@ -1,13 +1,23 @@
 import requests
 import os
+import re
 import yaml
 import time
 import platform
 import logging.handlers
+from pushover import client
 from xml.etree import ElementTree as ET
+from io import BytesIO
 
 # Identify platform
 print('Running code on: {}'.format(platform.system()))
+
+# Load configuration YAML
+path = os.path.dirname(os.path.realpath(__file__))
+cfg = yaml.load(open(path + '/config.yaml','r'))
+
+# Set up Pushover
+pushover = client(cfg['pushover']['token'], cfg['pushover']['user'], cfg['pushover']['url'])
 
 # Set up logging
 log_level = logging.INFO
@@ -24,31 +34,54 @@ logger.setLevel(log_level)
 if platform.system() == 'Linux':
     logger.addHandler(handler)
 
-# Load configuration YAML
-path = os.path.dirname(os.path.realpath(__file__))
-cfg = yaml.load(open(path + '/config.yaml','r'))
+def main():
+    # Initialize variables
+    lastMotionState = cfg['foscam']['state']['no alarm']
+    logger.info('Foscam and Pushover monitoring scripts activated.')
 
-# Initialize variables
-lastMotionState = cfg['foscam']['state']['no alarm']
+    while True:
+        # Sleep for 5 seconds
+        time.sleep(5)
 
-while True:
-    # Sleep for 5 seconds
-    time.sleep(5)
+        # Check for motion alarm
+        motionDetectAlarm = devstate()
+        if motionDetectAlarm:
+            if (lastMotionState == cfg['foscam']['state']['no alarm']) and (motionDetectAlarm == cfg['foscam']['state']['alarm']):
+                logger.warning('Alarm triggered!')
+                snap = snapshot()
+                if snap:
+                    pushover.message('Alarm triggered!', snapshot())
+                else:
+                    pushover.message('Alarm triggered, but unable to capture snapshot')
+            lastMotionState = motionDetectAlarm
 
+    logger.error('Foscam and Pushover monitoring scripts de-activated.')
+
+def devstate():
     # Read device status
     try:
-        fp = requests.get(cfg['foscam']['motor'])
+        cgi_url = cfg['foscam']['motor']['url'] + cfg['foscam']['motor']['cgi']
+        payload = {'usr': cfg['foscam']['motor']['user'], 'pwd': cfg['foscam']['motor']['pass'], 'cmd': 'getDevState'}
+        r = requests.get(cgi_url, params=payload)
+        root = ET.fromstring(r.text)    # Extract XML tree
+        return root.find('motionDetectAlarm').text
     except:
-        logger.warning('Not able to reach Foscam API')
-        continue
+        logger.warning('Unable to read device status [invalid response from Foscam API]')
+        return None
 
+def snapshot():
+    # Capture snapshot
     try:
-        root = ET.fromstring(fp.text) # Extract XML tree
-        motionDetectAlarm = root.find('motionDetectAlarm').text
-        if (lastMotionState == cfg['foscam']['state']['no alarm']) and (motionDetectAlarm == cfg['foscam']['state']['alarm']):
-            print('Alarm triggerred!')
-            logger.warning('Alarm triggered!')
-        lastMotionState = motionDetectAlarm
+        cgi_url = cfg['foscam']['motor']['url'] + cfg['foscam']['motor']['cgi']
+        payload = {'usr': cfg['foscam']['motor']['user'], 'pwd': cfg['foscam']['motor']['pass'], 'cmd': 'snapPicture'}
+        r = requests.get(cgi_url, params=payload)
+        reg = re.search('(Snap_\d{8}-\d{6}.jpg)',r.text)    # Get snapshot name: Snap_YYYYMMDD_HHMMSS.jpg
+        snap_url = cfg['foscam']['motor']['url'] + 'snapPic/' + reg.group(1)
+        r = requests.get(snap_url)
+        return r.content
     except:
-        logger.warning('Invalid data received from Foscam API')
-        continue
+        logger.warning('Unable to capture snapshot [invalid response from Foscam API]')
+        return None
+
+if __name__ == "__main__":
+    main()
---
+++ python/IPCam/readme.md
@@ -0,0 +1,26 @@
+# Foscam and Pushover
+
+## Introduction
+This script access a Foscam webcam and checks for motion detection. 
+Upon detected motion this is forwarded to a smartphone by using the Pushover service. 
+
+## Configuration file example
+The configuration file is not included in the repository, because it contains sensitive data. 
+To make these scripts work, create a config.yaml file and use the following layout.
+
+```python
+# Foscam configuration
+foscam:
+  motor       :
+    url       : 'http://YOURIPANDPORTHERE/'
+    cgi       : 'cgi-bin/CGIProxy.fcgi'
+    user      : 'YOURFOSCAMUSERNAMEHERE' # Foscam user needs admin rights to access getDevState!
+    pass      : 'YOURFOSCAMPASSWORDHERE'
+  state       :
+    disabled  : '0'
+    no alarm  : '1'
+    alarm     : '2'
+pushover:
+  url         : 'https://api.pushover.net/1/messages.json'
+  token       : 'YOURPUSHOVERTOKENHERE'
+  user        : 'YOURPUSHOVERUSERKEYHERE'
