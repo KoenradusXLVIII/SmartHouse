368a0c660c39843b99d1595bd33efe2e698daa9d
LAP-EHV-JV\Joost <joost.verberk@gmail.com>
2018-11-25 12:08:15 +0100

    Added Windows handler

--- python/logger/logger.py
+++ python/logger/logger.py
@@ -9,30 +9,36 @@ class Logger:
         self.log_level = log_level.lower()
 
         # Create logging instance
-        self.formatter = logging.Formatter('%(name)s [%(levelname)s] %(message)s')
         self.logger = logging.getLogger(self.name)
-        self.set_log_level(log_level)
+        self.formatter = logging.Formatter('%(name)s [%(levelname)s] %(message)s')
 
-        # Initialise and add logging handler (Linux only)
-        if platform.system() == 'Linux':
+        # Initialise and add logging handler (platform dependent)
+        if platform.system() == 'Windows':
+            self.handler = logging.StreamHandler()
+        elif platform.system() == 'Linux':
             self.handler = logging.handlers.SysLogHandler(address='/dev/log')
-            self.handler.setLevel(self.log_level)
-            self.handler.setFormatter(self.formatter)
-            self.logger.addHandler(self.handler)
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
+
+        # Set log level
+        self.set_log_level(log_level)
 
     def set_log_level(self, log_level):
         # Set new log level
         self.log_level = log_level.lower()
         if self.log_level == 'debug':
-            self.logger.setLevel(logging.DEBUG)
+            new_log_level = logging.DEBUG
         elif self.log_level == 'info':
-            self.logger.setLevel(logging.INFO)
+            new_log_level = logging.INFO
         elif self.log_level == 'warning':
-            self.logger.setLevel(logging.WARNING)
+            new_log_level = logging.WARNING
         elif self.log_level == 'error':
-            self.logger.setLevel(logging.ERROR)
+            new_log_level = logging.ERROR
         elif self.log_level == 'critical':
-            self.logger.setLevel(logging.CRITICAL)
+            new_log_level = logging.CRITICAL
+
+        self.logger.setLevel(new_log_level)
+        self.handler.setLevel(new_log_level)
 
     def get_log_level(self):
         # Return current log level
