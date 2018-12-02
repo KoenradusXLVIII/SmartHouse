2a7fcd24c1982a6b3d52daf28356061472e5a8ca
LAP-EHV-JV\Joost <joost.verberk@gmail.com>
2018-12-02 11:53:56 +0100

    Ready for new arduino code

--- python/arduino/arduino/arduino.py
+++ python/arduino/arduino/arduino.py
@@ -1,11 +1,4 @@
 import requests
-variables = {'temp': 'Temperature',
-             'humi': 'Humidity',
-             'rain': 'Rain',
-             'soil_humi': 'Soil Humidity',
-             'door_state': 'Door state',
-             'light_state': 'Light state',
-             'light_delay': 'Light delay'}
 
 
 class Client:
@@ -19,14 +12,12 @@ class Client:
         self.url = url
 
     def get_value(self,var):
-        if var in variables:
-            url = self.url + var
-            r = requests.get(url).json()
-            return r[variables[var]]
+        url = self.url + var.lower()
+        r = requests.get(url).json()
+        return r[var.lower()]
 
     def set_value(self, var, value):
-        if var in variables:
-            url = self.url + var + '/' + str(value)
-            r = requests.get(url).json()
-            if r[variables[var]] == int(value):
-                return value
+        url = self.url + var.lower() + '/' + str(value)
+        r = requests.get(url).json()
+        if r[var.lower()] == int(value):
+            return value
