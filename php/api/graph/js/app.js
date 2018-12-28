e18214463b5a497cbefd631880cb89d43f6b571f
LAP-EHV-JV\Joost <joost.verberk@gmail.com>
2018-12-28 10:18:50 +0100

    Javascript ready for multiple lines per axis!

--- php/api/graph/js/app.js
+++ php/api/graph/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	renderChart(3,4)
+	renderChart([3],[4,9])
 	$('#left_sensors').multiselect();
 	$('#right_sensors').multiselect();
 });
@@ -10,101 +10,145 @@ function renderChart(left_axis, right_axis) {
     	url: "http://www.joostverberk.nl/api/graph/data.php",
     	dataType:'json',
     	method: "POST",
-    	data: {sensors: [left_axis, right_axis]},
+    	data: {sensors: left_axis.concat(right_axis)},
     	success: function(data) {
-    		var timestamp = [];
-    		var left = [];
-    		var right = [];
 
-    		for(var i in data) {
-    			if(data[i].sensor_id==left_axis) {
-    			    timestamp.push(data[i].timestamp);
-    			    left.push(data[i].value);
-    			    left_quantity = data[i].quantity_name;
-    			    left_sensor = data[i].sensor_name;
-    			    left_uom = data[i].uom;
-    			}
-    			if(data[i].sensor_id==right_axis) {
-    			    right.push(data[i].value);
-    			    right_quantity = data[i].quantity_name;
-    			    right_uom = data[i].uom;
-    			    right_sensor = data[i].sensor_name;
-    			}
-    		}
-    
+            // Set colour sets
+            var colorset = ["#003f5c","#7a5195","#ef5675","#ffa600"];
+
+            // Set context
             var ctx = $("#graph-canvas");
+            // Initialize config
     		var config = {
-    			type: 'line',
-    			data:
-    			{
-    			    labels: timestamp,
-    			    datasets:
-    			    [{
-    				    label: left_sensor,
-    				    fill: false,
-    				    pointRadius: 0,
-    				    borderColor: "#c45850",
-    				    backgroundColor: "#c45850",
-    				    data: left,
-    				    yAxisID: 'y-axis-1'
-    			    },
-    			    {
-    				    label: right_sensor,
-    				    fill: false,
-    				    pointRadius: 0,
-    				    borderColor: "#3e95cd",
-    				    backgroundColor: "#3e95cd",
-    				    data: right,
-    				    yAxisID: 'y-axis-2'
-    			    }
-    			    ]
-    			},
-    			options: {
-    			    legend: {
-    			        display: true
-    			    },
-    			    tooltips: {
-    					position: 'nearest',
-    					mode: 'index',
-    					intersect: false,
-    				},
-                    responsive: true,
-                    scales: {
-                        xAxes: [{
-                            type: 'time',
-                            time: {
-                                unit: 'hour'
-                            }
-                        }],
-                        yAxes: [{
-                            id: 'y-axis-1',
-                            position: 'left',
-                            scaleLabel: {
-                                display: true,
-                                labelString: left_quantity + " [" + left_uom + "]",
-                            },
-                            ticks: {
-                                callback: function(value, index, values) {
-                                    return value + left_uom;
-                                }
-                            }
-                        },{
-                            id: 'y-axis-2',
-                            position: 'right',
-                            scaleLabel: {
-                                display: true,
-                                labelString: right_quantity + " [" + right_uom + "]",
-                            },
-                            ticks: {
-                                callback: function(value, index, values) {
-                                    return value + right_uom;
-                                }
-                            }
+    		    type: 'line',
+    		    data: {
+    		        labels: [],
+    		        datasets: []
+    		    }
+    		};
+
+    		// Initialize options
+    		var options = {
+			    legend: {
+			        display: true
+			    },
+			    tooltips: {
+					position: 'nearest',
+					mode: 'index',
+					intersect: false,
+				},
+                responsive: true,
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                            unit: 'hour'
                         }
-                        ]
-                    }
+                    }],
+                    yAxes: []
                 }
     		};
+
+    		// Initialize datasets in left axis
+    		sensor_loop:
+                for (var left_sensor_i = 0; left_sensor_i < left_axis.length; left_sensor_i++) {
+            meas_loop:
+                    for (var i1 in data) {
+                        if(data[i1].sensor_id == left_axis[left_sensor_i]) {
+                            if (left_sensor_i === 0) {
+                                // Get axis information from first dataset
+                                options.scales.yAxes.push({
+                                    id: 'y-axis-1',
+                                    position: 'left',
+                                    scaleLabel: {
+                                        display: true,
+                                        labelString: data[i1].quantity_name + " [" + data[i1].uom + "]",
+                                    },
+                                    ticks: {
+                                        callback: function(value, index, values) {
+                                            return value + data[i1].uom;
+                                        }
+                                    }
+                                });
+                            }
+                            // Get dataset information from each dataset
+                            config.data.datasets.push({
+                                label: data[i1].sensor_name,
+                                fill: false,
+                                pointRadius: 0,
+    				            borderColor: colorset[left_sensor_i],
+    				            backgroundColor: colorset[left_sensor_i],
+    				            data: [],
+    				            yAxisID: 'y-axis-1'
+                            });
+                            // Iterate to next sensor
+                            break meas_loop;
+                        }
+                    }
+                }
+
+            // Initialize datasets in right axis
+    		sensor_loop:
+                for (var right_sensor_i = 0; right_sensor_i < right_axis.length; right_sensor_i++) {
+            meas_loop:
+                    for (var i2 in data) {
+                        if(data[i2].sensor_id == right_axis[right_sensor_i]) {
+                            if (right_sensor_i === 0) {
+                                // Get axis information from first dataset
+                                options.scales.yAxes.push({
+                                    id: 'y-axis-2',
+                                    position: 'right',
+                                    scaleLabel: {
+                                        display: true,
+                                        labelString: data[i2].quantity_name + " [" + data[i2].uom + "]",
+                                    },
+                                    ticks: {
+                                        callback: function(value, index, values) {
+                                            return value + data[i2].uom;
+                                        }
+                                    }
+                                });
+                            }
+                            // Get dataset information from each dataset
+                            config.data.datasets.push({
+                                label: data[i2].sensor_name,
+                                fill: false,
+                                pointRadius: 0,
+    				            borderColor: colorset[left_axis.length + right_sensor_i],
+    				            backgroundColor: colorset[left_axis.length + right_sensor_i],
+    				            data: [],
+    				            yAxisID: 'y-axis-2'
+                            });
+                            // Iterate to next sensor
+                            break meas_loop;
+                        }
+                    }
+                }
+
+            // Attach options
+            config.options = options;
+
+            // Get timestamp information from first dataset
+            for (var meas_id in data) {
+                if(data[meas_id].sensor_id == left_axis[0]) {
+                    config.data.labels.push(data[meas_id].timestamp);
+                }
+            }
+            
+            // Populate datasets with data
+            all_sensors = left_axis.concat(right_axis);
+            for (var i3 in data) {
+                for (var all_sensor_i = 0; all_sensor_i < all_sensors.length; all_sensor_i++) {
+                    if(data[i3].sensor_id == all_sensors[all_sensor_i]) {
+                        config.data.datasets[all_sensor_i].data.push(data[i3].value);
+                        break;
+                    }
+                }
+            }
+
+            console.log(config);
+
+            // Draw chart
     		var lineGraph = new Chart(ctx, config);
     	},
     	error: function(response){
