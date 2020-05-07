f4ffdc572b719da9df14d04705b6a9a6942ad8eb
Joost Verberk <joostverberk@effectphotonics.nl>
2020-05-07 07:47:04 +0200

    Fixed IP getting for Raspbian

--- python/MQTT/scripts/ddns.py
+++ python/MQTT/scripts/ddns.py
@@ -8,6 +8,15 @@ import uuid
 import socket
 from requests import get
 
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    return ip_address
+
+
 # Main function
 def main():
     # Load configuration YAML
@@ -24,13 +33,13 @@ def main():
     mac = hex(uuid.getnode())
     mac = mac[2:].upper()
     hostname = socket.gethostname()
-    internal_ip = socket.gethostbyname(hostname)
+    internal_ip = get_ip_address()
     external_ip = get('https://api.ipify.org').text
 
     # Publish to MQTT
-    mqtt_client.publish('nodes/{}/hostname'.format(mac), hostname, retain=True)
-    mqtt_client.publish('nodes/{}/internal_ip'.format(mac), internal_ip, retain=True)
-    mqtt_client.publish('nodes/{}/external_ip'.format(mac), external_ip, retain=True)
+    mqtt_client.publish('{}/nodes/{}/hostname'.format(cfg['mqtt']['username'], mac), hostname, retain=True)
+    mqtt_client.publish('{}/nodes/{}/internal_ip'.format(cfg['mqtt']['username'], mac), internal_ip, retain=True)
+    mqtt_client.publish('{}/nodes/{}/external_ip'.format(cfg['mqtt']['username'], mac), external_ip, retain=True)
 
     # Blocking call that processes network traffic, dispatches callbacks and  handles reconnecting.
     mqtt_client.loop_start()
